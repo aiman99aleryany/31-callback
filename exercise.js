148e8adc0a404282495469451438e9960fd5ea14
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-22 00:49:25 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,13 +1,15 @@
 // Create two functions: the first one is called sayHelloName and takes is as parameter another function (the second one) called printName.
-// The printName function should just execute the console.log() method and print the name. The sayHelloName that takes in the callback as parameter, must execute the console.log() of the "Hello" string and after that invoke the function passed as parameter.
+// The printName function should just execute the console.log() method and print the name. The sayHelloName that takes in the callback as parameter,
+// must execute the console.log() of the "Hello" string and after that invoke the function passed as parameter.
 
 function sayHelloName(func) {
+  let name = "Aiman"
   console.log("Hello");
-  func();
+  func(name);
 }
 
 function printName(name) {
   console.log(name);
 }
 
-// * they didn't quite explain the task, so I assume that the sayHelloName takes 2 parameters, the name and the function, otherwise how are we supposed to get the name? but I only coded the required task, I didn't add anything extra.
+// I understand callbacks I just didn't understand the task :D, but now everything is good I suppose
